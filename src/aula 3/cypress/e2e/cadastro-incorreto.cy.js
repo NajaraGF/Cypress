a137e056d85430b387b510ba808d1b875b158417
Najha <najhateste@gmail.com>
2023-10-08 20:30:34 -0300

    Atualização

--- src/aula 3/cypress/e2e/cadastro-incorreto.cy.js
+++ src/aula 3/cypress/e2e/cadastro-incorreto.cy.js
@@ -3,7 +3,6 @@ describe('Página de cadastro', () => {
     beforeEach(() => {
         cy.visit('https://3076-cypress-alurapic-front.vercel.app/')
     })
-
     it('Verifica mensagens de campos obrigatórios na página de cadastro', () => {
         cy.get('[data-test="register"]').click();
         cy.get('[data-test="btnRegister"]').click();
--- src/aula 3/cypress/e2e/desafio-login.cy.js
+++ src/aula 3/cypress/e2e/desafio-login.cy.js
@@ -1,7 +1,7 @@
 describe('Página de login', () => {
 
     beforeEach(() => {
-        cy.visit('http://localhost:4200')
+        cy.visit('https://3076-cypress-alurapic-front.vercel.app/')
     })
 
         it('Deve preencher os campos do formulário corretamente e realizar login', () => {
