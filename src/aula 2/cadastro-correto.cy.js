56a9f32bcaff1d981d05a1afd0bde2249112bc46
Najha <najhateste@gmail.com>
2023-10-08 19:21:42 -0300

    Adicionando aula 1

---
+++ src/aula 2/cadastro-correto.cy.js
@@ -0,0 +1,11 @@
+describe('Página de cadastro', () => {
+  it('Preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
+    cy.visit('https://3076-cypress-alurapic-front.vercel.app/')
+    cy.contains('a','Register now').click();
+    cy.get('[data-test="email"]').type('najaguiland@email.com.br')
+    cy.get('[data-test="fullName"]').type('Najara Guiland Ferraz')
+    cy.get('[data-test="registerUserName"]').type('najaragf')
+    cy.get('[data-test="registerPassword"]').type('Nah280484')
+    cy.contains('button','Register').click();
+  })
+})
