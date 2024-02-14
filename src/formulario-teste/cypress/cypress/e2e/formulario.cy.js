70b864f17544e6c1791aaf44027d09fefb6b5ee8
Najha <najhateste@gmail.com>
2024-02-14 17:33:39 -0300

    Movendo o projeto formulário

--- src/formulario-teste/cypress/cypress/e2e/formulario.cy.js
+++ src/formulario-teste/cypress/cypress/e2e/formulario.cy.js
@@ -1,5 +1,5 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('file:///C:/Users/najag.LAPTOP-4L06GP1P/AppData/Local/Temp/fa350bbc-de9d-4285-b628-59de5ebe741d_formulario-teste.zip.41d/formulario-teste/index.html')
   })
 })
--- src/formulario-teste/cypress/e2e/formulario.cy.js
+++ src/formulario-teste/cypress/e2e/formulario.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('file:///C:/Users/najag.LAPTOP-4L06GP1P/AppData/Local/Temp/fa350bbc-de9d-4285-b628-59de5ebe741d_formulario-teste.zip.41d/formulario-teste/index.html')
+describe('Página de formulario', () => {
+  it('Preencher os campos do formulário corretamente para cadastrar um novo usuário', () => {
+    cy.visit('http://127.0.0.1:5500/index.html')
+      cy.contains('Enviar').click();
+  cy.get('[name="nome"]').type('Najara Guiland Ferraz')
+  cy.get('[name="email"]').type('najaguiland@email.com.br')
+  cy.get('[name="telefone"]').type('1198462-0000')
+  cy.get('[name="mensagem"]').type('Olá, meu nome é najara e gostaria solicitar o cadastro.')
+  cy.contains('Enviar').click();
+  cy.log();
   })
 })
